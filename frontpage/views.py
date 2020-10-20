f049335f2d8baf4caaedd9830b1b3cf71a8c89ee
Jamie C <coombes.jd@gmail.com>
2020-10-20 14:36:41 +0100

    Commit Friday's work, beginning of changes to use own view instead of online post image endpoint

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     template = loader.get_template('frontpage/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def blaimage(request):
+    if request.method != 'POST':
+        raise Http404('Only POSTs are allowed for this endpoint')
+
